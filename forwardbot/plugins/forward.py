e79b974b842441a3408c18fc664347f5f9c4aa5d
dhanushlegends787 <82709798+dhanushlegends787@users.noreply.github.com>
2021-05-10 21:48:53 -0700

    Update forward.py

--- forwardbot/plugins/forward.py
+++ forwardbot/plugins/forward.py
@@ -163,8 +163,9 @@ async def handler(event):
                                     fileename = fileename.replace(".mkv", "")
                                     print(dir(message.file))
                                     cap = str(f"**{fileename}\n\n⭕️ @HEVC_Moviesz\n\n⭕️ @Tamil_LinkzZ**")
+                                    capp = str(message.file.title)
                                     #━━━━━━━━━━━━━━━━\n🔘 @Ullu_Tamil\n🔘 @Tamil_Seriesz\n🔘 @HEVC_Moviesz\n🔘 @Tv_Web_Seriesz\n🔘 @Tamil_Old_Flims\n🔘 @Tamil_SerialsHD\n🔘 @Tamil_Cinebytes\n🔘 @Tamil_Paadalgal\n🔘 @Streaming_Moviez\n🔘 @Tamil_DUB_Movies\n🔘 @Actors_Filmography\n🔘 @Tamil_Programs_All\n🔘 @Tamil_BluRay_Movies\n🔘 @Tamil_4K_VideoSongs\n🔘 @Cook_With_Comali_Tamil\n➠ Group : 🎯\n💬 @Discussion_HD_Movies 💬\n━━━━━━━━━━━━━━━━━━━\n➠ Click Here More Channels\n       🌐 @Tamil_LinkzZ 🌐
-                                    await client.send_file(tochat, message.document, caption = cap)
+                                    await client.send_file(tochat, message.document, caption = capp)
                                     try:
                                         if len(str(message.file.name)) <= 95:
                                             print("Succesfully forwarded: " + str(message.file.name))
