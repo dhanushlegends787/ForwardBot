a3fa90c9b14a8d0ce6b569e9602cf286b0d0e727
dhanushlegends787 <82709798+dhanushlegends787@users.noreply.github.com>
2021-05-10 20:44:02 -0700

    Update forward.py

--- forwardbot/plugins/forward.py
+++ forwardbot/plugins/forward.py
@@ -158,8 +158,11 @@ async def handler(event):
                             try:
                                 if media_type(message) == 'Document':
                                     logmsg = str(message.file.name)
-                                    cap = str(f"{logmsg}**\n\n━━━━━━━━━━━━━━━━\n🔘 @Ullu_Tamil\n🔘 @Tamil_Seriesz\n🔘 @HEVC_Moviesz\n🔘 @Tv_Web_Seriesz\n🔘 @Tamil_Old_Flims\n🔘 @Tamil_SerialsHD\n🔘 @Tamil_Cinebytes\n🔘 @Tamil_Paadalgal\n🔘 @Streaming_Moviez\n🔘 @Tamil_DUB_Movies\n🔘 @Actors_Filmography\n🔘 @Tamil_Programs_All\n🔘 @Tamil_BluRay_Movies\n🔘 @Tamil_4K_VideoSongs\n🔘 @Cook_With_Comali_Tamil\n➠ Group : 🎯\n💬 @Discussion_HD_Movies 💬\n━━━━━━━━━━━━━━━━━━━\n➠ Click Here More Channels\n       🌐 @Tamil_LinkzZ 🌐**")
-                                    await client.send_file(tochat, message.document, caption = message.caption)
+                                    fileename = logmsg.replace("@HEVC_moviesz",  "")
+                                    fileename = fileename.replace("_",  " ")
+                                    cap = str(f"**{fileename}\n⭕️ @HEVC_Moviesz\n⭕️ @Tamil_LinkzZ**")
+                                    #━━━━━━━━━━━━━━━━\n🔘 @Ullu_Tamil\n🔘 @Tamil_Seriesz\n🔘 @HEVC_Moviesz\n🔘 @Tv_Web_Seriesz\n🔘 @Tamil_Old_Flims\n🔘 @Tamil_SerialsHD\n🔘 @Tamil_Cinebytes\n🔘 @Tamil_Paadalgal\n🔘 @Streaming_Moviez\n🔘 @Tamil_DUB_Movies\n🔘 @Actors_Filmography\n🔘 @Tamil_Programs_All\n🔘 @Tamil_BluRay_Movies\n🔘 @Tamil_4K_VideoSongs\n🔘 @Cook_With_Comali_Tamil\n➠ Group : 🎯\n💬 @Discussion_HD_Movies 💬\n━━━━━━━━━━━━━━━━━━━\n➠ Click Here More Channels\n       🌐 @Tamil_LinkzZ 🌐
+                                    await client.send_file(tochat, message.document, caption = cap)
                                     try:
                                         if len(str(message.file.name)) <= 95:
                                             print("Succesfully forwarded: " + str(message.file.name))
