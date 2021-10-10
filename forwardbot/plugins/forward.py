867a5a036490ed4c37d97183528bc3af645db373
dhanushlegends787 <82709798+dhanushlegends787@users.noreply.github.com>
2021-10-10 16:18:45 +0530

    Update forward.py

--- forwardbot/plugins/forward.py
+++ forwardbot/plugins/forward.py
@@ -167,8 +167,8 @@ async def handler(event):
                                     cap = str(f"**{fileename}")
                                     
                                     #capp = str(mess + "**\n\n━━━━━━━━━━━━━━━━\n🔘 @Ullu_Tamil\n🔘 @Tamil_Seriesz\n🔘 @HEVC_Moviesz\n🔘 @Tv_Web_Seriesz\n🔘 @Tamil_Old_Flims\n🔘 @Tamil_SerialsHD\n🔘 @Tamil_Cinebytes\n🔘 @Tamil_Paadalgal\n🔘 @Streaming_Moviez\n🔘 @Tamil_DUB_Movies\n🔘 @Actors_Filmography\n🔘 @Tamil_Programs_All\n🔘 @Tamil_BluRay_Movies\n🔘 @Tamil_4K_VideoSongs\n🔘 @Cook_With_Comali_Tamil\n➠ Group : 🎯\n💬 @Discussion_HD_Movies 💬\n━━━━━━━━━━━━━━━━━━━\n➠ Click Here More Channels\n       🌐 @Tamil_LinkzZ 🌐**")
-                                    caption = (str(message.text)).replace('_', '@Movie_Search_Pro_Bot')
-                                    caption = re.sub('@[^\s]+', '', caption)
+                                    caption = (str(message.text)).replace('_', ' ')
+                                    caption = re.sub('@[^\s]+', '@Movie_Search_Pro_Bot', caption)
                                    
                                     await client.send_file(tochat, message.document, caption = caption)
                                     try:
